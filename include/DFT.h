4af9084776eb157bb82640ccee4056c8bcc6b948
川上昌汰 <s2213566@coins.tsukuba.ac.jp>
2023-04-19 13:05:53 +0900

    vscodeでdebugできるように設定

--- include/DFT.h
+++ include/DFT.h
@@ -1,9 +1,4 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <complex.h>
-
 void DFT(int n, double complex* x, double complex *y, double complex *w);
 void invDFT(int n, double complex* x, double complex *y, double complex *w);
