803081c3a614deb0bd2ca29bc585d7ea0963310b
Kawakami Shota <kawakami.shota.0514@gmail.com>
2023-06-24 16:09:58 +0900

    setup: clang-formatの設定を変更

--- QD-FFT/include/util_calc.hpp
+++ QD-FFT/include/util_calc.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
 namespace util::calc {
-    void split(double a, double* a_hi, double* a_lo);
-    void quick_two_sum(double a, double b, double* s, double* e);
-    void two_sum(double a, double b, double* s, double* e);
-    void three_sum(double a, double b, double c, double* s, double* e0,
-                   double* e1);
-    void three_sum(double a, double b, double c, double* s, double* e0);
-    void two_prod(double a, double b, double* p, double* e);
-    void six_three_sum(double a, double b, double c, double d, double e,
-                       double f, double* s, double* e0, double* e1);
-    void dd_add_dd_dd(double a0, double a1, double b0, double b1, double* s0,
-                      double* s1);
-    void nine_two_sum(double a, double b, double c, double d, double e,
-                      double f, double g, double h, double i, double* s,
-                      double* e0);
+void split(double a, double* a_hi, double* a_lo);
+void quick_two_sum(double a, double b, double* s, double* e);
+void two_sum(double a, double b, double* s, double* e);
+void three_sum(double a, double b, double c, double* s, double* e0, double* e1);
+void three_sum(double a, double b, double c, double* s, double* e0);
+void two_prod(double a, double b, double* p, double* e);
+void six_three_sum(double a, double b, double c, double d, double e, double f,
+                   double* s, double* e0, double* e1);
+void dd_add_dd_dd(double a0, double a1, double b0, double b1, double* s0,
+                  double* s1);
+void nine_two_sum(double a, double b, double c, double d, double e, double f,
+                  double g, double h, double i, double* s, double* e0);
 }  // namespace util::calc
