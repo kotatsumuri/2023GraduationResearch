327e68430fa1638d804866a5ca3c5f9c3e573804
Kawakami Shota <kawakami.shota.0514@gmail.com>
2023-06-27 10:22:00 +0900

    feature: decimal_in_frequencyを実装

--- QD-FFT/src/fft.cpp
+++ QD-FFT/src/fft.cpp
@@ -1,8 +1,38 @@
 #include "fft.hpp"
 
+#include <iostream>
+
 #include "qd.hpp"
 
 namespace FFT {
+void make_cos_table(int N, QD::qd cos_table[]) {
+    QD::init(cos_table[0], 1.0);
+    if (N <= 2)
+        return;
+    int N_4 = N / 4;
+    QD::init(cos_table[N_4], 0.0);
+    if (N <= 4)
+        return;
+    int i = N_4 / 2;
+    int j;
+    QD::qd h;
+    QD::sqrt(2.0, cos_table[i]);
+    QD::mul_pwr2(cos_table[i], 0.5, cos_table[i]);
+    for (; i > 1; i /= 2) {
+        int i_2 = i / 2;
+        for (j = i; j < N_4; j += i * 2) {
+            QD::mul_pwr2(cos_table[j], 2, h);
+
+            QD::add(h, 2.0, cos_table[j + i_2]);
+            QD::sqrt(cos_table[j + i_2], cos_table[j - i_2]);
+            QD::mul_pwr2(cos_table[j - i_2], 0.5, cos_table[j - i_2]);
+
+            QD::sub(2.0, h, cos_table[j + i_2]);
+            QD::sqrt(cos_table[j + i_2], h);
+            QD::mul_pwr2(h, 0.5, cos_table[j + i_2]);
+        }
+    }
+}
 
 void decimal_in_frequency(int initN, int n, QD::qd x[], QD::qd ix[], QD::qd y[],
                           QD::qd iy[], QD::qd cos_table[]) {
@@ -10,8 +40,6 @@ void decimal_in_frequency(int initN, int n, QD::qd x[], QD::qd ix[], QD::qd y[],
         return;
 
     const int harf_n = n / 2;
-    int i            = 1;
-    int j            = 1 + harf_n;
     // y[0] = x[0] + x[n / 2]
     QD::add(x[0], x[harf_n], y[0]);
     QD::add(ix[0], ix[harf_n], iy[0]);
@@ -19,7 +47,9 @@ void decimal_in_frequency(int initN, int n, QD::qd x[], QD::qd ix[], QD::qd y[],
     QD::sub(x[0], x[harf_n], y[harf_n]);
     QD::sub(ix[0], ix[harf_n], iy[harf_n]);
 
+    int i = 1;
     if (n > 2) {
+        int j                 = 1 + harf_n;
         int i_                = harf_n - 1;
         int j_                = n - 1;
         const int harf_harf_n = harf_n / 2;
@@ -46,10 +76,9 @@ void decimal_in_frequency(int initN, int n, QD::qd x[], QD::qd ix[], QD::qd y[],
             QD::add(x[i_], x[j_], y[i_]);
             QD::add(ix[i_], ix[j_], iy[i_]);
 
-            QD::sub(x[i_], x[j_], y[j_]);
-            QD::sub(ix[i_], ix[j_], iy[j_]);
+            QD::sub(x[j_], x[i_], y[j_]);
+            QD::sub(ix[j_], ix[i_], iy[j_]);
 
-            QD::minus(a);
             QD::mul(y[j_], a, x[i_]);
             QD::mul(y[j_], b, ix[i_]);
             QD::mul(iy[j_], a, x[j_]);
@@ -63,18 +92,17 @@ void decimal_in_frequency(int initN, int n, QD::qd x[], QD::qd ix[], QD::qd y[],
             j_--;
         }
 
-        // y[0] = x[0] + x[n / 2]
+        // y[n / 4] = x[n / 4] + x[3 * n / 4]
         QD::add(x[i], x[j], y[i]);
         QD::add(ix[i], ix[j], iy[i]);
-        // y[n / 2] = x[0] - x[n / 2]
+        // y[3 * n / 4] = x[n / 4] - x[3 * n / 4]
         QD::sub(ix[i], ix[j], y[j]);
         QD::sub(x[j], x[i], iy[j]);
+
+        decimal_in_frequency(initN, harf_n, y, iy, x, ix, cos_table);
+        decimal_in_frequency(initN, harf_n, y + harf_n, iy + harf_n, x + harf_n,
+                             ix + harf_n, cos_table);
     }
-
-    decimal_in_frequency(initN, harf_n, y, iy, x, ix, cos_table);
-    decimal_in_frequency(initN, harf_n, y + harf_n, iy + harf_n, x + harf_n,
-                         ix + harf_n, cos_table);
-
     for (i = 0; i < harf_n; i++) {
         QD::copy(y[i], x[2 * i]);
         QD::copy(iy[i], ix[2 * i]);
--- QD-FFT/src/qd_io.cpp
+++ QD-FFT/src/qd_io.cpp
@@ -4,7 +4,7 @@
 #include "util_io.hpp"
 
 namespace QD {
-std::string to_bin_string(qd a) {
+std::string to_bin_string(const qd a) {
     return util::io::to_bin_string(a[0]) + "|" + util::io::to_bin_string(a[1]) +
            "|" + util::io::to_bin_string(a[2]) + "|" +
            util::io::to_bin_string(a[3]);
